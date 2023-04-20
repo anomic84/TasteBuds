101e640e58e145c3d4206c95331c9411cabb480f
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-04-20 14:10:22 -0700

    made basic structure for front end, including a working nav bar, and navbar mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Admin, Listings, User, Event, Home } from './pages'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import './app.css'
 
 function App() {
+  const [currentPage, setCurrentPage] = React.useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "Profile") {
+      return <Admin />;
+    }
+    if (currentPage === "Listings") {
+      return <Listings />;
+    }
+    return <Event />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App">
-     
-    </div>
+    // APP
+    <div className="w-screen h-screen">
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+
+      <main className="render-container">
+        {renderPage()}
+      </main>
+      <Footer />
+    </div >
+
+
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+// This index bottlenecks all the seperates into this one page to link to app.js
+export {default as Admin} from './Admin/Admin';
+export {default as Event} from './Event/Event';
+export {default as Home} from './Home/Home';
+export {default as User} from './User/User';
+export {default as Listings} from './Listings/Listings'
