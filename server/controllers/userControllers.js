463094fe268f9298a2a3d7f107d367aa1531f6c8
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-04-22 19:31:01 -0700

    added code to finish controllers, created schema, and utils with auth

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,17 +1,21 @@
 const { User } = require("../models");
+const { signToken } = require('../utils/auth')
 
 module.exports = {
   async createUser(req, res) {
-    User.create(req.body)
-      .then((user) => res.json(user))
+    await User.create(req.body)
+      .then((user) =>{ 
+        const token = signToken(user)
+        res.json({ token, user })}
+        )
       .catch((err) => res.json(err));
   },
 
   async getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
+    await User.findOne({ _id: req.params.userId })
       .then((user) =>
         !user
-          ? res.status(404).json({ message: "No user with found with that ID" })
+          ? res.status(404).json({ message: "No user with found with that ID." })
           : res.json({ user })
       )
       .catch((err) => {
@@ -21,6 +25,24 @@ module.exports = {
   },
 
   async login(req, res) {
-    
+      await User.findOne({ $or: [{ username: req.body.username}, { email: req.body.email }] })
+        .then((user) => 
+         !user
+           ? res.status(404).json({ message: "No user found with that username or email."})
+           : res.json({ user })
+        )
+        .catch((err) => {
+          console.log(err)
+          return res.status(500).json(err)
+        })
+
+      const correctPassword = await User.isCorrectPassword(req.body.password)
+
+      if(!correctPassword){
+        return res.status(400).json({ message: 'Wrong password!' })
+      }
+
+      const token = signToken(user)
+      res.json({ token, user })
   },
 };
