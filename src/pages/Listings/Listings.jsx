a8c345df97ea596e704ea247f5116984fb162fd7
C Martin <cmart131@yahoo.com>
2023-04-24 18:53:37 -0700

    added to user and listing pages

--- src/pages/Listings/Listings.jsx
+++ src/pages/Listings/Listings.jsx
@@ -1,31 +1,94 @@
 import React, { useEffect, useState } from 'react';
-import MessageSender from './MessageSender';
-import StoryReel from "./StoryReel";
-import Post from "./Post";
-import db from "../firebase"
+import EventCard from '../../components/EventCard'
+// import CreateModal from '../../components/CreateModal/CreateModal'
+import { allEvents } from '../../constants/constants'
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"
+import Loader from 'react-spinner-loader';
+import NavBar from '../../components/Navbar'
 
-function Listings() {
-    const [posts, setPosts] = useState([]);
+
+// function Listings() {
+//   return (
+//     <section className='h-[100%] w-full  flex flex-col justify-center'>
+//         <div className='m-4 border-4 border-borderblue mx-auto rounded-3xl my-4 p-4 drop-shadow-lg bg-card max-w-[600px]
+//         md:w-[80%]
+//         xl:max-w-[1000px]'>
+//             <h1 className='text-center text-borderblue font-manrope font-bold text-4xl
+//             xl:text-6xl xl:py-2'>Cryptic Andy</h1>
+//         </div>
+//         <div className='sm:m-4 flex flex-col items-center'>
+//             {allEvents.map((allEvent) => (
+//                 <EventCard key={allEvent.id} {...allEvent} />
+//             ))}
+//         </div>
+//         <div className='z-0'>
+//             <CreateModal />
+//         </div>
+//     </section>
+// )
+// }   
+
+
+// import { Link } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux"
+// import Loader from 'react-spinner-loader';
+// import NavBar from '../../components/Navbar'
+
+export const Listings = () => {
+
+    const {
+        auth: { token },
+        posts: { posts, isLoading },
+    } = useSelector( state => state );
+
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        db.collection('posts').orderBy("timestamp", "desc").onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }))
-            )))
-    })
+        dispatch(allEvents());
+    }, [dispatch, token]);
+
     return (
-        <div className="feed">
-            <StoryReel />
-            <MessageSender />
-            {posts.map(post => (
-                <Post
-                    key={post.id}
-                    profilePic={post.data.profilePic}
-                    message={post.data.message}
-                    timestamp={post.data.timestamp}
-                    username={post.data.username}
-                    image={post.data.image}
-                />
-            ))}
+        <div>
+            <NavBar />
+
+            <div className="flex justify-center px-5 sm:px-32 md:mt-4">
+                <div className="flex h-screen w-screen">
+
+                   
+
+                    <main className="md:mx-4 w-full sm:basis-2/3">
+
+                        <header className="m-4 hidden sm:flex">
+                            <h1 className="text-xl">All Posts</h1>
+                        </header>
+
+                        <header className="text-xl font-bold flex py-4 text-blue-600 sm:hidden">
+                            <Link to="/home" id="hero-logo"> TassteBuds </Link>
+                        </header>
+
+                        {isLoading ? (
+                            <div className="z-20">
+                                <Loader show={isLoading} />
+                            </div>
+                        ) : (
+                            posts.map(post => <EventCard key={post._id} post={EventCard} />)
+                        )}
+
+                    </main>
+
+                   
+                </div>
+            </div>
         </div>
-    );
-};
-export default Listings;
+    )
+}
+
+// export default Listings;
+
+
+
+
+
+
+
